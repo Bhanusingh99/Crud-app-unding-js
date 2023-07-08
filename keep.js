e372774c82c10d9a23ded3218cef880f3da28414
Bhanu singh <136106202+Bhanusingh99@users.noreply.github.com>
2023-07-09 00:07:39 +0530

    Update keep.js

--- keep.js
+++ keep.js
@@ -1,63 +1,57 @@
-// const circle = document.getElementById("circle");
-// const rotate = document.getElementById("rotate");
-// const makeItCenter = document.querySelector(".model");
-// const closeBtn = document.getElementById("closeBtn");
-// const deleteButton = document.getElementById("deleteButton");
-// const editButton = document.getElementById("editButton");
-// const title = document.getElementById("title");
-// const textarea = document.getElementById("textarea");
-// const form = document.getElementById("form");
-// const display = document.querySelector(".display");
+const circle = document.getElementById("circle");
+const rotate = document.getElementById("rotate");
+const makeItCenter = document.querySelector(".model");
+const closeBtn = document.getElementById("closeBtn");
+const deleteButton = document.getElementById("deleteButton");
+const editButton = document.getElementById("editButton");
+const title = document.getElementById("title");
+const textarea = document.getElementById("textarea");
+const form = document.getElementById("form");
+const display = document.querySelector(".display");
 
-// form.addEventListener("submit",(e)=>{
-//         e.preventDefault();
-//         puttingIntoLocalStorage();
-//         rotate.classList.remove("deg");
-//         makeItCenter.style.top = "-150%"
-//         })
+form.addEventListener("submit",(e)=>{
+        e.preventDefault();
+        puttingIntoLocalStorage();
+        rotate.classList.remove("deg");
+        makeItCenter.style.top = "-150%"
+        })
 
-// circle.addEventListener("click",()=>{
-//     makeitcenter();
-//     makeItCenter.style.top = "30%"
-// })
+circle.addEventListener("click",()=>{
+    makeitcenter();
+    makeItCenter.style.top = "30%"
+})
 
-// closeBtn.addEventListener("click",function(){
-//     makeItCenter.style.top = "-150%"
-//     rotate.classList.remove("deg");
-// })
+closeBtn.addEventListener("click",function(){
+    makeItCenter.style.top = "-150%"
+    rotate.classList.remove("deg");
+})
 
-// function makeitcenter(){
-//     rotate.classList.add("deg");
-//     makeItCenter.classList.add("makeItCenter");
-// }
-// // here i am deleting the element by clicking on delete button \\
-// function deleteelement(e){
-//     e.parentElement.parentElement.remove();
-// }
+function makeitcenter(){
+    rotate.classList.add("deg");
+    makeItCenter.classList.add("makeItCenter");
+}
+// here i am deleting the element by clicking on delete button \\
+function deleteelement(e){
+    e.parentElement.parentElement.remove();
+}
 
-// // Here i will put values of input to object \\
+// Here i will put values of input to object \\
 
-// const object ={};
-// function puttingIntoLocalStorage(){
-//     object["Title"] = title.value;
-//     object["inputText"] = textarea.value;
-//     display.innerHTML +=` <div>
-//     <span class="head">${object.Title}</span><br>
-//     <span class="bodyll">${object.inputText} </span>
-//     <main  id="forbutton">
-//      <img src="delete.png" alt="" height="18px" width="18px" id="deleteButton" onclick="deleteelement(this)">
-//      <img src="edit.png" alt="" height="18px" width="18px" id="editButton">
-//     </main>
-//   </div>`
+const object ={};
+function puttingIntoLocalStorage(){
+    object["Title"] = title.value;
+    object["inputText"] = textarea.value;
+    display.innerHTML +=` <div>
+    <span class="head">${object.Title}</span><br>
+    <span class="bodyll">${object.inputText} </span>
+    <main  id="forbutton">
+     <img src="delete.png" alt="" height="18px" width="18px" id="deleteButton" onclick="deleteelement(this)">
+     <img src="edit.png" alt="" height="18px" width="18px" id="editButton">
+    </main>
+  </div>`
      
 
-//     title.value = "";
-//     textarea.value = "";
-// }
-
-let str = "bhanu";
-let dbl ="";
-for(let i = str.length;i =>0;i--){
-    dbl += str[i];
+    title.value = "";
+    textarea.value = "";
 }
-console.log(dbl);
+
